9cb1aa2a8284b8d34be5e4ca8fa0a60d3a7d1c9a
Nicholas Lindboe <NicholasLindboe@gmail.com>
2021-09-07 21:14:26 +0200

    new mvc solution with basic routing

---
+++ ECommerceStore.View/Models/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceStore.View.Models
+{
+    public class Test
+    {
+    }
+}
